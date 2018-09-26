0199739c1742d596c165f4af131cdc28d3761458
Scammi <scammi@gmail.com>
2018-09-25 23:25:26 -0300

    scroll box

--- MiUDH/app.js
+++ MiUDH/app.js
@@ -8,7 +8,7 @@ function initMap() {
 //markers para hospitales         
 	   var infowindow = new google.maps.InfoWindow();
 	   for (var i =0; i < data.length; i++)
-       {  
+        {  
 		 var marker = new google.maps.Marker({
           position: new google.maps.LatLng(data[i].lat, data[i].long),
           map: map,
@@ -29,32 +29,33 @@ function initMap() {
   var markerz = [];
   searchBox.addListener('places_changed', function() {  
         var places = searchBox.getPlaces();          
-                    
           if (places.length == 0) {
             return;
           }
+          markerz.forEach(function(marker) {
+            marker.setMap(null);
+          });
           places.forEach(function(place) {
             if (!place.geometry) {
               console.log("Returned place contains no geometry");
               return;
             }
-            markerz.push(new google.maps.Marker({
-              map: map,
-              title: place.name,
-              position: place.geometry.location
+          markerz.push(new google.maps.Marker({
+            map: map,
+            title: place.name,
+            position: place.geometry.location
             }));
         //Calcula distancia de usuario a hospital
-              for (p = 0 ; p < data.length ; p++){
-            var hospiCord = new google.maps.LatLng({lat: data[p].lat, lng: data[p].long});
-            data[p].dis=(google.maps.geometry.spherical.computeDistanceBetween (place.geometry.location, hospiCord)); 
+    for (p = 0 ; p < data.length ; p++){
+        var hospiCord = new google.maps.LatLng({lat: data[p].lat, lng: data[p].long});
+        data[p].dis=(google.maps.geometry.spherical.computeDistanceBetween (place.geometry.location, hospiCord)); 
                }
         //ordenar de menor a mat             
-            data.sort(function(a, b) {
-            return a.dis - b.dis;});
+    data.sort(function(a, b) {
+        return a.dis - b.dis;});
              });            
         });  
     
-   
     //dropown list con hospitales
     /*
        var select =  document.getElementById('drop');
@@ -66,11 +67,12 @@ function initMap() {
     */
      }
     // prints to list 
-        function myFunction() {  
-            for (var u=0; u<data.length;u++){
-                 var node = document.createElement("LI");
-                 var textnode = document.createTextNode(data[u].Nombre);
-                 node.appendChild(textnode);
-                 document.getElementById("list").appendChild(node);
-                }
-            }
+    function myFunction() {
+        var list = document.getElementById("list")
+        document.getElementById("bot").style.overflow = "scroll"
+        for (var u=0; u<data.length;u++){
+             var node = document.createElement("LI");
+             var textnode = document.createTextNode(data[u].Nombre);
+             node.appendChild(textnode);
+             list.appendChild(node);
+        }}
