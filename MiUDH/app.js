eb8d408f94b5cfedf907884c983bd54cc3f10eae
Scammi <scammi@gmail.com>
2018-09-28 22:49:04 -0300

    UI simplifado y intento de cor. bug

--- MiUDH/app.js
+++ MiUDH/app.js
@@ -3,9 +3,10 @@ function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.603722, lng: -58.381592},
           zoom: 13,
-          styles: estilo
+          styles: estilo,
+          disableDefaultUI: true
         });
-//markers para hospitales         
+    //markers para hospitales         
 	   var infowindow = new google.maps.InfoWindow();
 	   for (var i =0; i < data.length; i++)
         {  
@@ -14,7 +15,7 @@ function initMap() {
           map: map,
           icon: 'icon-hs.png'
 		 });
-//infowindow para hospitales    
+    //infowindow para hospitales    
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
           infowindow.setContent(data[i].Nombre);
@@ -45,30 +46,23 @@ function initMap() {
             title: place.name,
             position: place.geometry.location
             }));
-        //Calcula distancia de usuario a hospital
-    for (p = 0 ; p < data.length ; p++){
+    
+             });            
+        });}
+    //Calcula distancia de usuario a hospital
+function CalcuOrden(){
+     for (p = 0 ; p < data.length ; p++){
         var hospiCord = new google.maps.LatLng({lat: data[p].lat, lng: data[p].long});
         data[p].dis=(google.maps.geometry.spherical.computeDistanceBetween (place.geometry.location, hospiCord)); 
                }
         //ordenar de menor a mat             
     data.sort(function(a, b) {
         return a.dis - b.dis;});
-             });            
-        });  
-    
-    //dropown list con hospitales
-    /*
-       var select =  document.getElementById('drop');
-    for (var l=0; l<data.length;l++){
-       var opt = document.createElement('option');
-       opt.text = data[l].nombre;
-       select.appendChild(opt);
-       }
-    */
-     }
+}
+
     // prints to list 
-    function myFunction() {
-        var list = document.getElementById("list")
+function myFunction() {
+    var list = document.getElementById("list")
         document.getElementById("bot").style.overflow = "scroll"
         for (var u=0; u<data.length;u++){
              var node = document.createElement("LI");
